f9b9b00c684e01f5252f3c34db78abbc8857891b
GeraldOKoth <okothgerald449@gmail.com>
2025-07-20 20:01:36 +0300

    style navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
-      <Link to="/">
-        
-      </Link>
-      
+    <div style={{backgroundColor: '#f3f3f3', display: 'flex', justifyContent: 'space-around'}}>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/contact">Contact</Link>
+      {/* <Link to="/not-found">Not Found</Link> */}
     </div>
   );
 }
