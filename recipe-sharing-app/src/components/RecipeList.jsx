a7e34e8607dbe0524bd5f9bc98b62f1b923c51e2
GeraldOKoth <okothgerald449@gmail.com>
2025-08-04 13:01:42 +0300

    fix: move zustand store to components folder

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 // RecipeList component
-import useRecipeStore  from "../store/useRecipeStore";
+import useRecipeStore from "./useRecipeStore";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
