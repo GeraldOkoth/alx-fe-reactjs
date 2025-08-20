52831dd6663d8f888d4f56e26666434fa74c23bc
GeraldOKoth <okothgerald449@gmail.com>
2025-08-20 12:01:40 +0300

    Set up basic structure of react app

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>API Intergration</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
