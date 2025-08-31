15724992eed99fa2869c84991e237bc88d09eaf7
GeraldOKoth <okothgerald449@gmail.com>
2025-08-31 15:31:07 +0300

    Configure tailwindcss and its dependencies

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
 
