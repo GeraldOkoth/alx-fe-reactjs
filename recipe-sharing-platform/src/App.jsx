4e8505c6f26643f5248ac48f435d88a117a19314
GeraldOKoth <okothgerald449@gmail.com>
2025-08-31 16:49:18 +0300

    Uncomment code

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
-// import RecipeDetail from "./components/RecipeDetail"; 
+import RecipeDetail from "./components/RecipeDetail"; 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/recipes/:id" element={<RecipeDetail />} /> */}
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
       </Routes>
     </Router>
   );
